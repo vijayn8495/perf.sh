bccdc37f27ab1d7589ef899c8e30f295c20c59f0
FelipeFMA <felipefmavelar@gmail.com>
2025-03-19 14:52:27 -0300

    use case instead of if

--- perf.sh
+++ perf.sh
@@ -22,37 +22,42 @@ Options:
   -l      (low)    Set power-saver profile and GPU min/max frequency to 300 MHz
   --help           Show this help message"
 
-if [ "$1" == "--help" ]; then
-    echo "$HELP_MESSAGE"
-    exit 0
-fi
-
 if [[ $EUID -ne 0 ]]; then
    echo "This script requires root privileges."
    sudo "$0" "$@"
    exit 0
 fi
 
-if [ "$1" == "-h" ]; then
-    powerprofilesctl set performance
-    echo 1100 > /sys/class/drm/card0/gt_min_freq_mhz
-    echo "Performance mode enabled"
-    exit 0
-elif [ "$1" == "-m" ]; then
-    powerprofilesctl set balanced
-    echo "If a error shows up, don't worry, everything is fine."
-    echo 550 > /sys/class/drm/card0/gt_max_freq_mhz
-    echo 550 > /sys/class/drm/card0/gt_min_freq_mhz
-    echo 550 > /sys/class/drm/card0/gt_max_freq_mhz
-    echo "Power-saver mode (medium) enabled"
-    exit 0
-elif [ "$1" == "-l" ]; then
-    powerprofilesctl set power-saver
-    echo 300 > /sys/class/drm/card0/gt_min_freq_mhz
-    echo 300 > /sys/class/drm/card0/gt_max_freq_mhz
-    echo "Power-saver mode (low) enabled"
-    exit 0
-else
-    echo "$HELP_MESSAGE"
-    exit 1
-fi
+case "$1" in
+    "--help")
+        echo "$HELP_MESSAGE"
+        exit 0
+        ;;
+    "-h")
+        powerprofilesctl set performance
+        echo 1100 > /sys/class/drm/card0/gt_max_freq_mhz
+        echo 1100 > /sys/class/drm/card0/gt_min_freq_mhz
+        echo "Performance mode enabled"
+        exit 0
+        ;;
+    "-m")
+        powerprofilesctl set balanced
+        echo "If a error shows up, don't worry, everything is fine."
+        echo 550 > /sys/class/drm/card0/gt_max_freq_mhz
+        echo 550 > /sys/class/drm/card0/gt_min_freq_mhz
+        echo 550 > /sys/class/drm/card0/gt_max_freq_mhz
+        echo "Power-saver mode (medium) enabled"
+        exit 0
+        ;;
+    "-l")
+        powerprofilesctl set power-saver
+        echo 300 > /sys/class/drm/card0/gt_min_freq_mhz
+        echo 300 > /sys/class/drm/card0/gt_max_freq_mhz
+        echo "Power-saver mode (low) enabled"
+        exit 0
+        ;;
+    *)
+        echo "$HELP_MESSAGE"
+        exit 1
+        ;;
+esac
